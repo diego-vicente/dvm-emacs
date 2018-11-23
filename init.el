2fd1fff21c1af74fe04d2a65e2ae3b4138c0aa58
Diego Vicente <mail@diego.codes>
2018-11-23 16:56:46 +0100

    Improve the default init.el

--- init.el
+++ init.el
@@ -1,6 +1,14 @@
+;; Initialize package
+(package-initialize)
+
 ;; Loads everything from the real configuration
 (if (file-directory-p "~/nixos-setup")
 	(setq configuration-dir "~/nixos-setup/my-emacs/")
   (setq configuration-dir "~/my-emacs/"))
 
+;; Autoload use-package
+(eval-when-compile
+  (require 'use-package))
+
+;; Load the literate configuration
 (org-babel-load-file (concat configuration-dir "README.org"))
